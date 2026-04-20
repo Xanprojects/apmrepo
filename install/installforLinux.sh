4efe9296ac69f377a440fd78120ce9a515863570
Xanprojects <ljwaluaprojects@gmail.com>
2026-04-20 20:49:51 +0800

    Update installforLinux.sh

--- install/installforLinux.sh
+++ install/installforLinux.sh
@@ -2,6 +2,7 @@
 
 echo "Downloads file..."
 cd ~
-wget https://github.com/Xanprojects/apmrepo/raw/refs/heads/main/install/src/apmlinux > /dev/null 2>&1
+wget https://github.com/Xanprojects/apmrepo/raw/refs/heads/main/install/src/apmlinux --quiet
 mv apmlinux /usr/local/bin/apm
+chmod +x /usr/local/bin/apm
 echo "Success"
